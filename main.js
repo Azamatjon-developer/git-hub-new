66e26fa527b511786d72e3f2de2fcd033d929602
Azamatjon-developer <azamatergasev01@gmail.com>
2025-10-17 15:13:45 +0500

    done task

---
+++ main.js
@@ -0,0 +1,12 @@
+// Typed.js orqali matn animatsiyasi yaratish
+let typed = new Typed("#typed-text", {
+  strings: [
+    "Azamatjon Mustafo Nazarbek Hojiakbar MuhammadAli",
+    "Animation starts",
+    "MODERN DESIGN",
+    "CREATIVE EFFECTS",
+  ],
+  typeSpeed: 80, // Harf yozilish tezligi
+  backSpeed: 90, // O‘chirish tezligi
+  loop: true, // Doimiy aylanishi
+});
